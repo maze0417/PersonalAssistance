0592cbc9874378f4542bf5570bf1ea41d76f0992
maze.chang <maze.chang@afusiom.com>
2018-06-28 19:31:01 +0800

    update

--- PunchCard/Program.cs
+++ PunchCard/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.ServiceProcess;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.WindowsServices;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PunchCard.Clients;
+using PunchCard.Services;
 
 namespace PunchCard
 {
@@ -41,13 +45,22 @@ namespace PunchCard
 
             if (isService)
             {
-                host.RunAsService();
+                host.RunAsCustomService();
             }
             else
             {
                 host.Run();
                 Console.WriteLine("Run As Console");
             }
+        }
+    }
+
+    public static class WebHostServiceExtensions
+    {
+        public static void RunAsCustomService(this IWebHost host)
+        {
+            var webHostService = new CustomWebHostService(host, host.Services.GetService<IHrResourceClient>());
+            ServiceBase.Run(webHostService);
         }
     }
 }
