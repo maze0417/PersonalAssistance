cf831acd093cde7b1c652ddb51322fa9ffe06057
maze.chang <maze.chang@afusiom.com>
2018-06-29 18:28:37 +0800

    refactor

--- PunchCard/Startup.cs
+++ PunchCard/Startup.cs
@@ -24,7 +24,7 @@ namespace PunchCard
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime applicationLifetime)
         {
             if (env.IsDevelopment())
             {
@@ -46,6 +46,10 @@ namespace PunchCard
             });
             var hrResourceService = app.ApplicationServices.GetService<IHrResourceService>();
             hrResourceService.Init();
+            applicationLifetime.ApplicationStopping.Register(() =>
+            {
+                hrResourceService.PunchCardAsync().GetAwaiter().GetResult();
+            });
         }
     }
 }
