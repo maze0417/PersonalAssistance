9b114c6904757476ce264911671dc4ae6dac0101
maze.chang <maze.chang@afusiom.com>
2018-07-02 12:19:14 +0800

    init

---
+++ PunchCardService/Clients/BaseApiClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Core.Clients
+{
+    public abstract class BaseApiClient
+    {
+        private const int TimeoutInSeconds = 15;
+        private static readonly HttpClient HttpClient = new HttpClient();
+        private readonly ILogger _logger;
+
+        protected BaseApiClient(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        protected async Task<TResponse> SendAsync<TResponse>(HttpRequestMessage request)
+        {
+            try
+            {
+                using (var response = await HttpClient.SendAsync(request, new CancellationTokenSource(TimeSpan.FromSeconds(TimeoutInSeconds)).Token))
+                {
+                    var res = await response.Content.ReadAsStringAsync();
+                    _logger.LogDebug(res);
+                    if (response.StatusCode != HttpStatusCode.OK)
+                    {
+                        _logger.LogWarning(res);
+                        return default;
+                    }
+
+                    return JsonConvert.DeserializeObject<TResponse>(res);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return default;
+            }
+        }
+    }
+}
---
+++ PunchCardService/Models/GetDaCardDetailResponse.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+// ReSharper disable InconsistentNaming
+
+namespace Core.Models
+{
+    public class Data
+    {
+        public string dateString { get; set; }
+        public int status { get; set; }
+        public string statusString { get; set; }
+        public string timeStart { get; set; }
+        public string timeEnd { get; set; }
+        public List<string> cardTime { get; set; }
+    }
+
+    public class GetDaCardDetailResponse
+    {
+        public bool success { get; set; }
+        public string code { get; set; }
+        public string message { get; set; }
+        public List<Data> data { get; set; }
+        public string errorCode { get; set; }
+    }
+
+    public class GetDaCardDetailRequest
+    {
+        public string cid { get; set; }
+        public string pid { get; set; }
+        public string date { get; set; }
+    }
+}
